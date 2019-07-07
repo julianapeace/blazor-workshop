73164c869c2cf6047330fcb843d2a577222dc796
julianapeace <chancecordelia@gmail.com>
2019-07-07 03:40:43 -0500

    trying to figure out JSRuntime LocalStorage bug

---
+++ save-points/00-Starting-point/BlazingPizza.Client/ServerAuthenticationStateProvider.cs
@@ -0,0 +1,30 @@
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazingPizza.Client
+{
+    public class ServerAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly HttpClient _httpClient;
+
+        public ServerAuthenticationStateProvider(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var userInfo = await _httpClient.GetJsonAsync<UserInfo>("user");
+
+            var identity = userInfo.IsAuthenticated
+            ? new ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, userInfo.Name) }, "serverauth")
+            : new ClaimsIdentity();
+
+            // var claim = new Claim(ClaimTypes.Name, "fake user");
+            // var identity = new ClaimsIdentity(new[] { claim }, "serverauth");
+            return new AuthenticationState(new ClaimsPrincipal(identity));
+        }
+    }
+}
